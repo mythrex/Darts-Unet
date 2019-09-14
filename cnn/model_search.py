540f205ce0e77208f3aff0c4981bfd25f08e5dd1
Shivam Kaushik <shivam.kaushik73@gmail.com>
2019-09-14 23:04:33 +0530

    Network works in Session mode

--- cnn/model_search.py
+++ cnn/model_search.py
@@ -1,13 +1,12 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from operations import *
-from torch.autograd import Variable
+import tensorflow as tf
+from tensorflow.keras import Model
 from genotypes import PRIMITIVES
 from genotypes import Genotype
+from operations import *
+import numpy as np
 
 
-class MixedOp(nn.Module):
+class MixedOp(Model):
     """Makes mixed operations object
     """
 
@@ -18,14 +17,17 @@ class MixedOp(nn.Module):
             C (int): the current state
         """
         super(MixedOp, self).__init__()
-        self._ops = nn.ModuleList()
+        self._ops = []
         for primitive in PRIMITIVES:
-            op = OPS[primitive](C, stride, False)
-            if 'pool' in primitive:
-                op = nn.Sequential(op, nn.BatchNorm2d(C, affine=False))
-            self._ops.append(op)
-
-    def forward(self, x, weights):
+                op = OPS[primitive](C, stride)
+                if 'pool' in primitive:
+                    if('avg' in primitive):
+                        op = AvgPool3x3(C, stride)
+                    elif('max' in primitive):
+                        op = MaxPool3x3(C, stride)
+                self._ops.append(op)
+        
+    def call(self, x, weights):
         """Converts the discrete set of operation into conitnuous mixed operation
 
         Args:
@@ -34,66 +36,63 @@ class MixedOp(nn.Module):
 
         Returns:
             tensor: sum of product(weights, operation(x))
-        """ 
+        """
         return sum(w * op(x) for w, op in zip(weights, self._ops))
 
+class Cell(Model):
 
-class Cell(nn.Module):
+    def __init__(self, steps, multiplier, C_prev_prev, C_prev, C, reduction, reduction_prev, upsample_prev):
+        super(Cell, self).__init__()
+        self.reduction = reduction
 
-  def __init__(self, steps, multiplier, C_prev_prev, C_prev, C, reduction, reduction_prev, upsample_prev):
-    super(Cell, self).__init__()
-    self.reduction = reduction
+        if reduction_prev:
+            self.preprocess0 = FactorizedReduce(C_prev_prev, C)
+        elif upsample_prev:
+            self.preprocess0 = FactorizedUp(C_prev_prev, C)
+        else:
+            self.preprocess0 = ReLUConvBN(C_prev_prev, C, 1, 1, 'same')
+        self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 'same')
+        self._steps = steps
+        self._multiplier = multiplier
 
-    if reduction_prev:
-      self.preprocess0 = FactorizedReduce(C_prev_prev, C, affine=False)
-    elif upsample_prev:
-      self.preprocess0 = FactorizedUp(C_prev_prev, C, affine=False)
-    else:
-      self.preprocess0 = ReLUConvBN(C_prev_prev, C, 1, 1, 0, affine=False)
-    self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0, affine=False)
-    self._steps = steps
-    self._multiplier = multiplier
+        self._ops = []
+        self._bns = []
+        for i in range(self._steps):
+            for j in range(2+i):
+                stride = 2 if reduction and j < 2 else 1
+                op = MixedOp(C, stride)
+                self._ops.append(op)
 
-    self._ops = nn.ModuleList()
-    self._bns = nn.ModuleList()
-    for i in range(self._steps):
-      for j in range(2+i):
-        stride = 2 if reduction and j < 2 else 1
-        op = MixedOp(C, stride)
-        self._ops.append(op)
-
-  def forward(self, s0, s1, weights):
+    def call(self, s0, s1, weights):
     
-    s0 = self.preprocess0(s0)
-    s1 = self.preprocess1(s1)
+        s0 = self.preprocess0(s0)
+        s1 = self.preprocess1(s1)
+
+        states = [s0, s1]
+        offset = 0
     
-    states = [s0, s1]
-    offset = 0
-    for i in range(self._steps):
-      s = sum(self._ops[offset+j](h, weights[offset+j]  ) for j, h in enumerate(states))
-      offset += len(states)
-      states.append(s)
+        for i in range(self._steps):
+            s = sum(self._ops[offset+j](h, weights[offset+j]) for j, h in enumerate(states))
+            offset += len(states)
+            states.append(s)
 
-    return torch.cat(states[-self._multiplier:], dim=1)
+        return tf.concat(states[-self._multiplier:], axis=-1)
 
-class UpsampleCell(nn.Module):
+class UpsampleCell(Model):
 
     def __init__(self, steps, multiplier, C_prev_prev, C_prev, C):
         super(UpsampleCell, self).__init__()
-        self.preprocess0 = ReLUConvBN(C_prev_prev, C, 1, 1, 0, affine=False)
-        self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 0, affine=False)
+        self.preprocess0 = ReLUConvBN(C_prev_prev, C, 1, 1, 'same')
+        self.preprocess1 = ReLUConvBN(C_prev, C, 1, 1, 'same')
         self._steps = steps
         self._multiplier = multiplier
-        self.UpConv = nn.ConvTranspose2d(C, 
-                                        C*self._multiplier, 
+        self.UpConv = layers.Conv2DTranspose(C*self._multiplier, 
                                         kernel_size=3,
-                                        stride=2,
-                                        padding=1,
-                                        output_padding=1,
-                                        dilation=1)
+                                        strides=2,
+                                        padding='same')
         self.reduction = False
 
-    def forward(self, s0, s1, weights):
+    def call(self, s0, s1, weights):
         s0 = self.preprocess0(s0)
         s1 = self.preprocess1(s1)
 
@@ -102,150 +101,140 @@ class UpsampleCell(nn.Module):
 
         return s0 + s1
 
-class Network(nn.Module):
+class Network(Model):
+    
+    def __init__(self, C, net_layers, criterion, steps=4, multiplier=4, stem_multiplier=3):
+        super(Network, self).__init__()
+        self._C = C
+        self._criterion = criterion
+        self._steps = steps
+        self._multiplier = multiplier
+        self.net_layers = net_layers
 
-  def __init__(self, C, num_classes, layers, criterion, steps=4, multiplier=4, stem_multiplier=3):
-    super(Network, self).__init__()
-    self._C = C
-    self._num_classes = num_classes
-    self._layers = layers
-    self._criterion = criterion
-    self._steps = steps
-    self._multiplier = multiplier
-    self._layers = layers
+        C_curr = C*stem_multiplier
+
+        # stem operation
+        self.stem_op = tf.keras.Sequential()
+        self.stem_op.add(tf.keras.layers.Conv2D(C_curr, kernel_size=3, padding='same', use_bias=False))
+        self.stem_op.add(tf.keras.layers.BatchNormalization())
+
+        C_prev_prev, C_prev, C_curr = C_curr, C_curr, C
+        self.cells = []
+
+        reduction_prev = False
+        # For reduction
+        for i in range(self.net_layers):
+            if i % 2 == 1:
+                C_curr *= 2
+                reduction = True
+            else:
+                reduction = False
+
+            cell = Cell(steps, multiplier, C_prev_prev, C_prev, C_curr,
+                        reduction,
+                        reduction_prev,
+                        upsample_prev=False)
+            reduction_prev = reduction
+            self.cells += [cell]
+
+            C_prev_prev, C_prev = C_prev, multiplier*C_curr
+
+        for i in range(self.net_layers-1):
+            if i % 2 == 0:
+                C_curr = C_curr // 2
+
+                cell = UpsampleCell(steps, multiplier, C_prev_prev, C_prev, C_curr)
+            else:
+                cell = Cell(steps, multiplier, C_prev_prev, C_prev, C_curr,
+                            reduction=False,
+                            reduction_prev=False,
+                            upsample_prev=True)
+            self.cells += [cell]
+            C_prev_prev, C_prev = C_prev, multiplier*C_curr
+
+        self.softmaxConv = tf.keras.Sequential()
+        self.softmaxConv.add(tf.keras.layers.Conv2D(1, kernel_size=1, strides=1, padding='same'))
+        self.softmaxConv.add(Softmax())
+
+        self._initialize_alphas()
     
-    C_curr = C*stem_multiplier
-    self.stem = nn.Sequential(
-      nn.Conv2d(3, C_curr, 3, padding=1, bias=False),
-      nn.BatchNorm2d(C_curr)
-    )
-    
-    C_prev_prev, C_prev, C_curr = C_curr, C_curr, C
-    self.cells = nn.ModuleList()
-    reduction_prev = False
-    for i in range(layers):
-      if i % 2 == 1:
-        C_curr *= 2
-        reduction = True
+    def _initialize_alphas(self):
+        k = sum(1 for i in range(self._steps) for n in range(2+i))
+        num_ops = len(PRIMITIVES)
         
-      else:
-        reduction = False
-        
-      cell = Cell(steps, multiplier, C_prev_prev, C_prev, C_curr,
-                  reduction,
-                  reduction_prev,
-                  upsample_prev=False)
-      reduction_prev = reduction
-      self.cells += [cell]
-      C_prev_prev, C_prev = C_prev, multiplier*C_curr
+        self.alphas_normal = tf.Variable(1e-3*tf.random.uniform([k, num_ops]), name='alphas_normal')
+        self.alphas_reduce = tf.Variable(1e-3*tf.random.uniform([k, num_ops]), name='alphas_reduce')
+        self._arch_parameters = [
+          self.alphas_normal,
+          self.alphas_reduce,
+        ]
     
-    for i in range(layers-1):
-      if i % 2 == 0:
-        C_curr = C_curr // 2
-        
-        cell = UpsampleCell(steps, multiplier, C_prev_prev, C_prev, C_curr)
-      else:
-        
-        cell = Cell(steps, multiplier, C_prev_prev, C_prev, C_curr,
-                    reduction=False,
-                    reduction_prev=False,
-                    upsample_prev=True)
-      self.cells += [cell]
-      C_prev_prev, C_prev = C_prev, multiplier*C_curr
+    def arch_parameters(self):
+        return self._arch_parameters
     
-    self.sigmoidConv = nn.Sequential(
-            nn.Conv2d(C_prev,
-                      1,
-                      kernel_size=1,
-                      stride=1,
-                      padding=0
-                      ),
-            nn.Sigmoid()
-    )
+    def new(self):
+        model_new = Network(self._C, self.net_layers, self._criterion)
+        for x, y in zip(model_new.arch_parameters(), self.arch_parameters()):
+            x.assign(y)
+        return model_new
     
-    self._initialize_alphas()
-
-  def new(self):
-    model_new = Network(self._C, self._num_classes, self._layers, self._criterion).cuda()
-    for x, y in zip(model_new.arch_parameters(), self.arch_parameters()):
-        x.data.copy_(y.data)
-    return model_new
-
-  def forward(self, input):
-    s0 = s1 = self.stem(input)
-    self.arr = []
-    ids = []
-    pos = -1
+    def _loss(self, logits, target):
+        return self._criterion(logits, tf.to_float(target))
     
-    middle = self._layers - 1
-    
-    for i, cell in enumerate(self.cells):
-      if cell.reduction:
-        weights = F.softmax(self.alphas_reduce, dim=-1)
-      else:
-        weights = F.softmax(self.alphas_normal, dim=-1)
-      
-      s0, s1 = s1, cell(s0, s1, weights)
-    #   print("Cell no: {}, s1: {}".format(i, s1.shape))
-      
-      if (i < middle and i % 2 == 0):
-        self.arr.append(s1)
-        ids.append(i)
-      
-      if (i > middle and i % 2 == 1):
-        # print("Skip Connection on Cell no {} and {}".format(ids[pos], i))
-        C_curr = s1.shape[1]
-        op = SkipConnection(C_curr).cuda()
-        s1 = op(self.arr[pos], s1)
-        pos -= 1
+    def call(self, inp):
+        s0 = s1 = self.stem_op(inp)
+        self.arr = []
+        ids = []
+        pos = -1
 
-    return self.sigmoidConv(s1)
+        middle = self.net_layers - 1
+        for i, cell in enumerate(self.cells):
+            if cell.reduction:
+                weights = tf.nn.softmax(self.alphas_reduce, axis=-1)
+            else:
+                weights = tf.nn.softmax(self.alphas_normal, axis=-1)
 
-  def _loss(self, input, target):
-    logits = self(input)
-    return self._criterion(logits, target) 
+            s0, s1 = s1, cell(s0, s1, weights)
 
-  def _initialize_alphas(self):
-    k = sum(1 for i in range(self._steps) for n in range(2+i))
-    num_ops = len(PRIMITIVES)
+            if (i < middle and i % 2 == 0):
+                self.arr.append(s1)
+                ids.append(i)
 
-    self.alphas_normal = Variable(1e-3*torch.randn(k, num_ops).cuda(), requires_grad=True)
-    self.alphas_reduce = Variable(1e-3*torch.randn(k, num_ops).cuda(), requires_grad=True)
-    self._arch_parameters = [
-      self.alphas_normal,
-      self.alphas_reduce,
-    ]
+            if (i > middle and i % 2 == 1):
+                C_curr = s1.shape[1]
+                op = SkipConnection(C_curr)
+                s1 = op(self.arr[pos], s1)
+                pos -= 1
 
-  def arch_parameters(self):
-    return self._arch_parameters
+        return self.softmaxConv(s1)
 
-  def genotype(self):
+    def genotype(self):
 
-    def _parse(weights):
-      gene = []
-      n = 2
-      start = 0
-      for i in range(self._steps):
-        end = start + n
-        W = weights[start:end].copy()
-        edges = sorted(range(i + 2), key=lambda x: -max(W[x][k] for k in range(len(W[x])) if k != PRIMITIVES.index('none')))[:2]
-        for j in edges:
-          k_best = None
-          for k in range(len(W[j])):
-            if k != PRIMITIVES.index('none'):
-              if k_best is None or W[j][k] > W[j][k_best]:
-                k_best = k
-          gene.append((PRIMITIVES[k_best], j))
-        start = end
-        n += 1
-      return gene
+        def _parse(weights):
+            gene = []
+            n = 2
+            start = 0
+            for i in range(self._steps):
+                end = start + n
+                W = weights[start:end].copy()
+                edges = sorted(range(i + 2), key=lambda x: -max(W[x][k] for k in range(len(W[x])) if k != PRIMITIVES.index('none')))[:2]
+                for j in edges:
+                    k_best = None
+                    for k in range(len(W[j])):
+                        if k != PRIMITIVES.index('none'):
+                            if k_best is None or W[j][k] > W[j][k_best]:
+                                k_best = k
+                    gene.append((PRIMITIVES[k_best], j))
+                start = end
+                n += 1
+            return gene
 
-    gene_normal = _parse(F.softmax(self.alphas_normal, dim=-1).data.cpu().numpy())
-    gene_reduce = _parse(F.softmax(self.alphas_reduce, dim=-1).data.cpu().numpy())
+            gene_normal = _parse(tf.nn.softmax(self.alphas_normal, axis=-1).numpy())
+            gene_reduce = _parse(tf.nn.softmax(self.alphas_reduce, dim=-1).numpy())
 
-    concat = range(2+self._steps-self._multiplier, self._steps+2)
-    genotype = Genotype(
-      normal=gene_normal, normal_concat=concat,
-      reduce=gene_reduce, reduce_concat=concat
-    )
-    return genotype
+            concat = range(2+self._steps-self._multiplier, self._steps+2)
+            genotype = Genotype(
+            normal=gene_normal, normal_concat=concat,
+            reduce=gene_reduce, reduce_concat=concat
+            )
+            return genotype
--- cnn/utils.py
+++ cnn/utils.py
@@ -1,110 +1,122 @@
 import os
 import numpy as np
-import torch
-import shutil
-from torch.autograd import Variable
+import tensorflow as tf
 
-def accuracy(output, target):
-    batch_size = target.size(0)
-    h = target.size(2)
-    w = target.size(3)
+# def get_tensor_at(tensor, i):
+#     msk = np.identity(tensor.shape[0])[i]
+#     return tf.boolean_mask(tensor, msk)
 
-    res = ((output > 0.5).float() == target.float()).sum()
-    return res.float() / (batch_size * h * w)
+# def get_tensor_at(tensor, i):
+#     msk = tf.eye(tensor.shape[0])
+#     return tf.boolean_mask(tensor, msk)
 
+def get_tensor_at(tensor, msk, i):
+    return tf.ragged.boolean_mask(tensor, msk[i])
 
-def iou(output, target):
-    """Return IoU, intersection, union
+# def get_tensor_at(tensor, msk, i):
+#     return tf.random_uniform([1], 0, 255)
 
-    Args:
-        output (tensor): logits/output tensor
-        target (tensor): label/target tensor
+# def accuracy(output, target):
+#     batch_size = target.size(0)
+#     h = target.size(2)
+#     w = target.size(3)
 
-    Returns:
-        (tensor, tensor, tensor): iou, intersection, union
-    """
-    output = (output > 0.5).squeeze(1)
-    target = (target == 1).squeeze(1)
-    smoothy = 1e-6
-
-    intersection = (output & target).sum().float()
-    union = (output | target).sum().float()
-
-    iou = (intersection)/(union + smoothy)
-
-    return iou, intersection, union
+#     res = ((output > 0.5).float() == target.float()).sum()
+#     return res.float() / (batch_size * h * w)
 
 
-class Cutout(object):
-    def __init__(self, length):
-        self.length = length
+# def iou(output, target):
+#     """Return IoU, intersection, union
 
-    def __call__(self, img):
-        h, w = img.size(1), img.size(2)
-        mask = np.ones((h, w), np.float32)
-        y = np.random.randint(h)
-        x = np.random.randint(w)
+#     Args:
+#         output (tensor): logits/output tensor
+#         target (tensor): label/target tensor
 
-        y1 = np.clip(y - self.length // 2, 0, h)
-        y2 = np.clip(y + self.length // 2, 0, h)
-        x1 = np.clip(x - self.length // 2, 0, w)
-        x2 = np.clip(x + self.length // 2, 0, w)
+#     Returns:
+#         (tensor, tensor, tensor): iou, intersection, union
+#     """
+#     output = (output > 0.5).squeeze(1)
+#     target = (target == 1).squeeze(1)
+#     smoothy = 1e-6
 
-        mask[y1: y2, x1: x2] = 0.
-        mask = torch.from_numpy(mask)
-        mask = mask.expand_as(img)
-        img *= mask
-        return img
+#     intersection = (output & target).sum().float()
+#     union = (output | target).sum().float()
 
+#     iou = (intersection)/(union + smoothy)
 
-def count_parameters_in_MB(model):
-    return np.sum(np.prod(v.size()) for name, v in model.named_parameters() if "auxiliary" not in name)/1e6
+#     return iou, intersection, union
 
 
-def save_checkpoint(state, is_best, save):
-    filename = os.path.join(save, 'checkpoint.pth.tar')
-    torch.save(state, filename)
-    if is_best:
-        best_filename = os.path.join(save, 'model_best.pth.tar')
-        shutil.copyfile(filename, best_filename)
+# class Cutout(object):
+#     def __init__(self, length):
+#         self.length = length
+
+#     def __call__(self, img):
+#         h, w = img.size(1), img.size(2)
+#         mask = np.ones((h, w), np.float32)
+#         y = np.random.randint(h)
+#         x = np.random.randint(w)
+
+#         y1 = np.clip(y - self.length // 2, 0, h)
+#         y2 = np.clip(y + self.length // 2, 0, h)
+#         x1 = np.clip(x - self.length // 2, 0, w)
+#         x2 = np.clip(x + self.length // 2, 0, w)
+
+#         mask[y1: y2, x1: x2] = 0.
+#         mask = torch.from_numpy(mask)
+#         mask = mask.expand_as(img)
+#         img *= mask
+#         return img
 
 
-def save(model, model_path):
-    torch.save(model, model_path)
-
-def load(model, model_path):
-    torch.load(model_path)
+# def count_parameters_in_MB(model):
+#     return np.sum(np.prod(v.size()) for name, v in model.named_parameters() if "auxiliary" not in name)/1e6
 
 
-def drop_path(x, drop_prob):
-    if drop_prob > 0.:
-        keep_prob = 1.-drop_prob
-        mask = Variable(torch.cuda.FloatTensor(
-            x.size(0), 1, 1, 1).bernoulli_(keep_prob))
-        x.div_(keep_prob)
-        x.mul_(mask)
-    return x
+# def save_checkpoint(state, is_best, save):
+#     filename = os.path.join(save, 'checkpoint.pth.tar')
+#     torch.save(state, filename)
+#     if is_best:
+#         best_filename = os.path.join(save, 'model_best.pth.tar')
+#         shutil.copyfile(filename, best_filename)
 
 
-def create_exp_dir(path, scripts_to_save=None):
-    if not os.path.exists(path):
-        os.mkdir(path)
-    print('Experiment dir : {}'.format(path))
+# def save(model, model_path):
+#     torch.save(model, model_path)
 
-    if scripts_to_save is not None:
-        os.mkdir(os.path.join(path, 'scripts'))
-        for script in scripts_to_save:
-            dst_file = os.path.join(path, 'scripts', os.path.basename(script))
-            shutil.copyfile(script, dst_file)
+# def load(model, model_path):
+#     torch.load(model_path)
 
-def write_genotype(genotype, file_name="final_genotype"):
-    cwd = os.getcwd()
-    path = os.path.join(cwd, 'cnn', 'final_model')
+
+# def drop_path(x, drop_prob):
+#     if drop_prob > 0.:
+#         keep_prob = 1.-drop_prob
+#         mask = Variable(torch.cuda.FloatTensor(
+#             x.size(0), 1, 1, 1).bernoulli_(keep_prob))
+#         x.div_(keep_prob)
+#         x.mul_(mask)
+#     return x
+
+
+# def create_exp_dir(path, scripts_to_save=None):
+#     if not os.path.exists(path):
+#         os.mkdir(path)
+#     print('Experiment dir : {}'.format(path))
+
+#     if scripts_to_save is not None:
+#         os.mkdir(os.path.join(path, 'scripts'))
+#         for script in scripts_to_save:
+#             dst_file = os.path.join(path, 'scripts', os.path.basename(script))
+#             shutil.copyfile(script, dst_file)
+
+# def write_genotype(genotype, file_name="final_genotype"):
+#     cwd = os.getcwd()
+#     path = os.path.join(cwd, 'cnn', 'final_model')
     
-    if(not os.path.exists(path)):
-        os.mkdir(os.path.join(cwd, 'cnn', 'final_model'))
+#     if(not os.path.exists(path)):
+#         os.mkdir(os.path.join(cwd, 'cnn', 'final_model'))
     
-    comment = '# This file is auto-generated. This file contains the model searched through train_search'
+#     comment = '# This file is auto-generated. This file contains the model searched through train_search'
     
-    with open(os.path.join(path, '{}.py'.format(file_name)), 'w+') as f:
-        f.write("{}\n\nfrom collections import namedtuple\n\nGenotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')\n\ngenotype={}".format(comment, genotype))
+#     with open(os.path.join(path, '{}.py'.format(file_name)), 'w+') as f:
+#         f.write("{}\n\nfrom collections import namedtuple\n\nGenotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')\n\ngenotype={}".format(comment, genotype))
